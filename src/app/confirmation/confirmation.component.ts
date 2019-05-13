93ebf2d259d0fc021d31022c623a0927fbb89960
juhiaxovel <juhi.axovel@gmail.com>
2019-05-13 18:22:45 +0530

    change note text and add placeholder

--- src/app/confirmation/confirmation.component.ts
+++ src/app/confirmation/confirmation.component.ts
@@ -4,20 +4,26 @@
 * showed to the user on submission of form
 */
 import { Component, OnInit } from '@angular/core';
-import { trigger, transition, animate, style } from '@angular/animations';
+import { trigger, transition, animate, style, state } from '@angular/animations';
 
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
   styleUrls: ['./confirmation.component.css'],
   animations: [
-    trigger('slideInOut', [
-      transition(':enter', [style({ opacity: 0 }), animate('0.6s', style({ opacity: 1 }))])
+    trigger('openClose', [
+      state('void => *', style({ background: 'red' })),
+      state('* => *', style({ background: 'green' })),
+      transition('void => *', animate(2000)),
+      transition('void => *', animate('100ms ease-in')),
+      transition('inactive => active', animate('100ms ease-out'))
     ])
-  ]
+  ],
 })
 export class ConfirmationComponent implements OnInit {
   buttonText = 'Save this number';
+  isDisabled = false;
+  isOpen = false;
 
   constructor() { }
 
@@ -38,6 +44,7 @@ export class ConfirmationComponent implements OnInit {
     document.execCommand('copy');
     document.body.removeChild(selBox);
     this.buttonText = 'Copied!';
+    
   }
 
 }
