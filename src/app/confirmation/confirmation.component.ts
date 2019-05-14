a19993228389c266bcd09c954d0c792fb7dfb66e
Anand Kashyap <ak@cloudify.biz>
2019-05-14 10:07:49 +0530

    css changes

--- src/app/confirmation/confirmation.component.ts
+++ src/app/confirmation/confirmation.component.ts
@@ -4,26 +4,14 @@
 * showed to the user on submission of form
 */
 import { Component, OnInit } from '@angular/core';
-import { trigger, transition, animate, style, state } from '@angular/animations';
 
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
-  styleUrls: ['./confirmation.component.css'],
-  animations: [
-    trigger('openClose', [
-      state('void => *', style({ background: 'red' })),
-      state('* => *', style({ background: 'green' })),
-      transition('void => *', animate(2000)),
-      transition('void => *', animate('100ms ease-in')),
-      transition('inactive => active', animate('100ms ease-out'))
-    ])
-  ],
+  styleUrls: ['./confirmation.component.css']
 })
 export class ConfirmationComponent implements OnInit {
   buttonText = 'Save this number';
-  isDisabled = false;
-  isOpen = false;
 
   constructor() { }
 
@@ -44,7 +32,6 @@ export class ConfirmationComponent implements OnInit {
     document.execCommand('copy');
     document.body.removeChild(selBox);
     this.buttonText = 'Copied!';
-    
   }
 
 }
